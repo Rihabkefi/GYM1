facb65a78ef2f68529fc26306e3325c6bc5e84fe
Rihabkefi <rihabkefi40@gmail.com>
2024-04-07 12:11:38 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.gym {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.gym to javafx.fxml;
+    exports com.example.gym;
+}
